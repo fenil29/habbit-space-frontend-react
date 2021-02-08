c8226805560a196ddc5d77ed6b4048780946ab6c
fenil29 <fenilkaneria99@gmail.com>
2021-02-08 13:34:30 +0530

    add backend call to login with google api

--- src/component/home/login/Login.jsx
+++ src/component/home/login/Login.jsx
@@ -1,13 +1,26 @@
-import React from "react";
-import { Center, Box, Divider,Button,Icon } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Center, Box, Divider, Button, Icon } from "@chakra-ui/react";
 import "./Login.scss";
 import { GoogleLogin } from "react-google-login";
-import { FcGoogle } from "react-icons/fc"
-
+import { FcGoogle } from "react-icons/fc";
+import axios from "axios";
 const responseGoogle = (response) => {
   console.log(response);
+  axios.post("http://localhost:5555/api/login-with-google", {
+    token: response.tokenId,
+  }).then((response) => {
+    console.log(response);
+  }, (error) => {
+    console.log(error);
+  });;
 };
 function Login() {
+  useEffect(() => {
+    // effect
+    return () => {
+      // cleanup
+    };
+  }, []);
   return (
     <Box className="login">
       <Center>
@@ -22,10 +35,13 @@ function Login() {
           onFailure={responseGoogle}
           cookiePolicy={"single_host_origin"}
           render={(renderProps) => (
-            <Button colorScheme="blue" variant="outline" 
-            leftIcon={<Icon as={FcGoogle} />}
-            className="google-login-button"
-             onClick={renderProps.onClick}>
+            <Button
+              colorScheme="blue"
+              variant="outline"
+              leftIcon={<Icon as={FcGoogle} />}
+              className="google-login-button"
+              onClick={renderProps.onClick}
+            >
               Login with Google
             </Button>
           )}
