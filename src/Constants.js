59dac9517582e56176fc524035f459651ecba1e7
fenil29 <fenilkaneria@gmail.com>
2021-09-02 11:15:12 +0530

    change google oauth client id

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
 // Constants.js
 // export const API_URL: "" // Production
 export const API_URL= '' // Development
-export const GOOGLE_AUTH_CLIENT_ID= '973934553610-iph5uagr02rc6rgkcee7jloq3q7qpdpa.apps.googleusercontent.com' // Development
+export const GOOGLE_AUTH_CLIENT_ID= '16225194121-t3mi80msga3dk8ngf9bdbjt3hb1frbfp.apps.googleusercontent.com' // Development
