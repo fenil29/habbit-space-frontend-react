e8a26700dae39ad9bd708d5443b277b073f25634
fenil29 <fenilkaneria@gmail.com>
2021-09-06 21:11:48 +0530

    add icon in navbar

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Logo.scss";
+import Rocket from "../assets/rocket.svg";
 
 function Logo() {
-  return <div className="logo">Habbit Space</div>;
+  return (
+    <div className="logo">
+      <img src={Rocket} alt="Home Illustration" className="icon"/>
+      Habbit Space
+    </div>
+  );
 }
 
 export default Logo;
